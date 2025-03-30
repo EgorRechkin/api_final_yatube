a2143041b694126dddf8f1fc68a09220520237fa
Egor <egorrechkin1@gmail.com>
2025-03-30 17:11:26 +0700

    Поправка

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied, ValidationError, NotFound
 from django.shortcuts import get_object_or_404
 from posts.models import Post, Comment, Follow, Group
-from .serializers import PostSerializer, CommentSerializer, FollowSerializer, GroupSerializer
+from .serializers import PostSerializer, CommentSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
